06713617aa27ac1d5687a053f56c553776caf122
mukthar-am <mukthar.am@gmail.com>
2016-05-31 01:02:41 +0530

    parser records

--- src/test/java/com/aconex/converter/GedcomConverterTests.java
+++ src/test/java/com/aconex/converter/GedcomConverterTests.java
@@ -3,6 +3,7 @@ package com.aconex.converter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
@@ -13,6 +14,7 @@ public class GedcomConverterTests {
     @Test(groups = "converter-tests", expectedExceptions = FileNotFoundException.class)
     public void TestInputFileDoesNotExists() throws FileNotFoundException {
         System.out.println("# Input file does not exists.");
+
         String invalidInput = "/Users/mukthar.ahmed/Downloads/input.txt";
 
         boolean converter = new GedcomConverter(invalidInput).convert();
@@ -20,13 +22,31 @@ public class GedcomConverterTests {
         Assert.assertTrue(false);
     }
 
-    @Test(groups = "converter-tests")
+    @Test(groups = "converter-tests", enabled = false)
     public void TestInputFileNotReadable() {
         String nonReadableInput = "/Users/mukthar.ahmed/Data/git/personal/aconex-assignment/aconex-xml-util/non-readable.txt";
         System.out.println("# Input file is not readable for the user.");
 
         try {
             boolean converterStatus = new GedcomConverter(nonReadableInput).convert();
+            System.out.println("# Return value: " + converterStatus);
+            Assert.assertFalse(converterStatus);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(groups = "converter-tests")
+    public void TestValidInputFile() {
+        //String nonReadableInput = "/Users/mukthar" +
+        String validInput = "com/acconex/test-data/limited_data_set.txt";
+
+        String filepath = this.getClass().getClassLoader().getResource(validInput).getFile().toString();
+        System.out.println("# Input file is not readable for the user = " + filepath);
+
+        try {
+            boolean converterStatus = new GedcomConverter(filepath).convert();
             System.out.println("# Return value: " + converterStatus);
             Assert.assertFalse(converterStatus);
 
